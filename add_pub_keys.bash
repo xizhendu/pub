a7cc1a27ec232e0214e3cec0478ecece1a722c3b
Xizhen Du <xizhendu@gmail.com>
2020-08-16 13:31:07 +0800

    2020-08-16

--- add_pub_keys.bash
+++ add_pub_keys.bash
@@ -4,7 +4,7 @@
 
 mkdir -p ~/.ssh
 
-cat >> authorized_keys << _eof_
+cat >> ~/.ssh/authorized_keys << _eof_
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjG3KUxtu6HWgB7rgoZWjXQqB2X+2C+Qe0Usbcf+i784rfYuXIu4L6MG2YlNDtUjylna0JNd1vAuwQXY1DXj4Xo0I/TgJcIvyXWFOTYcLX1PI/u0s4XMmu9btjrd+d5VbR/cahDz0KqxQN79vbLW863CNXFEb+EIKhy++hyb2BCHp1t14N1LSvOAad/NnGT+w7u92X+pO5TUtLrX/Y4rrS4OV0TKQE18eL4CdBA25OsA0suUvJBdgI+HGSw//fBh+2U1BJa0SvbFMnCDlo/H+s+gCc2460W8oN897TP/ZiyOmPJYgbynmrXYGoxCwuJFCmL2QaxNJWFrI68XwrnFjR xizhendu@keystone.predix.int.ge.com
 _eof_
 
